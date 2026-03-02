3401ff410e8c6c40ec3ff37f5372360fc5deb6cb
BSA Intern <BSAIntern@ad.feedingsandiego.org>
2026-03-02 14:43:57 -0800

    Ver. 8.5.3 - I am dumb

--- charts.py
+++ charts.py
@@ -45,7 +45,7 @@ def make_entity_chart(df_entity: pd.DataFrame, granularity: str, include_prior: 
                 y=df_entity["lbs_prior"],
                 mode="lines+markers",
                 line=dict(color="orange", width=3),
-                marker=dict(color="orange", symbol="circle", width=4),
+                marker=dict(color="orange", symbol="circle", size=5),
                 name="Prior FY (same dates)",
                 hovertemplate="%{x}<br>%{y:,.0f} Lbs (Prior FY)<extra></extra>",
             )
